11f54a9925f9b33a6a7bcc16959c22dfc1979ac7
dinooka <dinooka7@gmail.com>
2025-09-15 08:48:30 +0530

    accessibility tests

---
+++ tests/accessibility-tests.spec.ts
@@ -0,0 +1,22 @@
+import { test, expect } from "@playwright/test";
+import AxeBuilder from "@axe-core/playwright";
+
+test.describe("Accessibility Issues", () => {
+	test("Accessibility issues in Home Page", async ({ page }) => {
+		await page.goto("/");
+		const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+		expect(accessibilityScanResults.violations).toEqual([]);
+	});
+	test("Accessibility issues for specific tags", async ({ page }, testInfo) => {
+		await page.goto("/");
+		const accessibilityScanResults = await new AxeBuilder({ page })
+			.withTags(["wcag2a", "wcag2aa", "wcag21a", "wcag21aa", "wcag22aa"])
+			.analyze();
+		await testInfo.attach("accessibility-scan-results", {
+			body: JSON.stringify(accessibilityScanResults, null, 2),
+			contentType: "application/json",
+		});
+
+		expect(accessibilityScanResults.violations).toEqual([]);
+	});
+});
